8a0cd4dbbbe0c6147c9dc0fc36daaf71325643d4
Victor <demeautis.victor@gmail.com>
2018-02-07 11:23:30 +0100

    creation d'un listener basic pour test

--- src/main/java/fr/istic/sit/config/RabbitMQConfiguration.java
+++ src/main/java/fr/istic/sit/config/RabbitMQConfiguration.java
@@ -3,6 +3,7 @@ package fr.istic.sit.config;
 import fr.istic.sit.rabbitMQ.Constants;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -17,7 +18,7 @@ public class RabbitMQConfiguration {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-            new CachingConnectionFactory("localhost",5672);
+            new CachingConnectionFactory("192.168.43.186",5672);
         System.out.println("ConnectionFactory : " + connectionFactory.toString());
         return connectionFactory;
     }
@@ -35,20 +36,20 @@ public class RabbitMQConfiguration {
         return rabbitTemplate;
     }
 
-        @Bean
-        public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-            return new Jackson2JsonMessageConverter();
-        }
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
     @Bean
     public Queue myQueue() {
         return new Queue(Constants.androidQueueName);
     }
 
-    /*@Bean(name="rabbitListenerContainerFactory")
+    @Bean(name="rabbitListenerContainerFactory")
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerlistenerFactory(){
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
         return factory;
-    }*/
+    }
 }
